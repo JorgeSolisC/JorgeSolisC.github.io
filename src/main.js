0d5c2193e17c5b1dfdbc424ceb6e6308d4767e07
Jorge Solis <jorge.solis@cceo.com.mx>
2025-05-31 14:23:56 -0600

    deps :heavy_plus_sign: Install dev dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createI18n} from "vue3-i18n";
+import en from './lang/en.json'
+import es from './lang/es.json'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    locale: localStorage.locale ?? 'en',
+    legacy: false,
+    globalInjection: true,
+    messages: {
+        en,
+        es
+    }
+});
+const app = createApp(App);
+app.use(i18n);
+app.mount('#app')
