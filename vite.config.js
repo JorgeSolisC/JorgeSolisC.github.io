48f8ac3569bd98ac1d11c922a8064be95f91d730
Jorge Solis <jorge.solis@cceo.com.mx>
2025-07-12 19:53:11 -0600

    ci :construction_worker: Configure GitHub Pages for CV deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
     plugins: [vue(), eslintPlugin()],
     resolve: {
         alias: {
